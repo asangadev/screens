4cc77bfe412a7e4d87d1943040586bccebae627d
Asanga Abeywickrama <Asanga.Abeywickrama@retailzoo.com.au>
2020-08-08 08:34:56 +1000

    init repo

---
+++ platforms/ios/www/cordova_plugins.js
@@ -0,0 +1,33 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+  module.exports = [
+    {
+      "id": "cordova-plugin-wkwebview-engine.ios-wkwebview-exec",
+      "file": "plugins/cordova-plugin-wkwebview-engine/src/www/ios/ios-wkwebview-exec.js",
+      "pluginId": "cordova-plugin-wkwebview-engine",
+      "clobbers": [
+        "cordova.exec"
+      ]
+    },
+    {
+      "id": "cordova-plugin-wkwebview-engine.ios-wkwebview",
+      "file": "plugins/cordova-plugin-wkwebview-engine/src/www/ios/ios-wkwebview.js",
+      "pluginId": "cordova-plugin-wkwebview-engine",
+      "clobbers": [
+        "window.WkWebView"
+      ]
+    },
+    {
+      "id": "cordova-uxcam.UXCam",
+      "file": "plugins/cordova-uxcam/www/UXCam.js",
+      "pluginId": "cordova-uxcam",
+      "clobbers": [
+        "UXCam"
+      ]
+    }
+  ];
+  module.exports.metadata = {
+    "cordova-plugin-whitelist": "1.3.4",
+    "cordova-plugin-wkwebview-engine": "1.2.1",
+    "cordova-uxcam": "3.3.0"
+  };
+});
